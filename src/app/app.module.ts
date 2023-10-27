3e0079a07039cf12cea1825dd882465fdcfa66c6
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-10-27 06:42:27 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
